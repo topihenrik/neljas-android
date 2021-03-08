d938e119c6d41c17b144eac7a6bef59cfe86291f
topii <topi.ilmoni@gmail.com>
2021-03-08 18:52:50 +0200

    Versio 1.0

---
+++ app/src/main/java/com/example/neljasandroid/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.neljasandroid;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+    TextView text;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = findViewById(R.id.textView1);
+        editText = findViewById(R.id.editText1);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                text.setText(editText.getText().toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+
+}
